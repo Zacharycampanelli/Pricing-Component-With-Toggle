a374d8d967466a57c3663398820508d739a3c4a8
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-04-24 20:39:21 -0400

    basic content layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./assets/css/input.css"],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+   
+    extend: {
+      
+      colors: {
+        veryLightGrayishBlue: `hsl(var(--veryLightGrayishBlue), <alpha-value>)`,
+        lightGrayishBlue: 'hsl(var(--lightGrayishBlue), <alpha-value>)',
+        grayishBlue: 'hsl(var(--grayishBlue), <alpha-value>)',
+        darkGrayishBlue: 'hsl(var(--darkGrayishBlue), <alpha-value>)',
+        gradientStart: 'hsl(var(--gradientStart), <alpha-value>)',
+        gradientEnd: 'hsl(var(--gradientEnd), <alpha-value>)',
+      },
+
+    },
+ 
   },
   plugins: [],
-}
-
+};
