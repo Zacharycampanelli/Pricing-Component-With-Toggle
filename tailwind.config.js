aa7f0e68dee243d72f00a7ea74c94d2aa314f470
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-04-25 18:37:07 -0400

    mobile-annual layout finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-   
+    fontSize: {
+      base: '0.938rem',
+      h1: '2rem',
+      h2: '1.125rem',
+      pricing: '4.5rem',
+      dollar: '2.5rem',
+      button: '0.813rem'
+    },
     extend: {
-      
       colors: {
         veryLightGrayishBlue: `hsl(var(--veryLightGrayishBlue), <alpha-value>)`,
         lightGrayishBlue: 'hsl(var(--lightGrayishBlue), <alpha-value>)',
@@ -13,9 +19,7 @@ module.exports = {
         gradientStart: 'hsl(var(--gradientStart), <alpha-value>)',
         gradientEnd: 'hsl(var(--gradientEnd), <alpha-value>)',
       },
-
     },
- 
   },
   plugins: [],
 };
